3b4165e724e04099e7715e21c6d5b50c46a4f22f
Kyle <kyle.latino@gmail.com>
2017-10-05 15:53:28 -0700

    logic issue..
    Still not getting the correct result.
    Checking issues w/ recursion.
    
    ...suggest 'rubber ducky' debugging

--- Summing_the_N_series.java
+++ Summing_the_N_series.java
@@ -3,24 +3,22 @@ import java.util.*;
 
 public class Summing_the_N_series {
 
-        // methods
-        // find the sum of the series and print to stdout
-        int solve(int input){
-                if (input<2){
-                        return 1;
-                }else{
-                        return (input^2 - (solve(input-1))^2);
-                }
-        }
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int testCases = sc.nextInt();
         
-        // main function
-        public static void main(String[] args){
-   
-                Scanner sc = new Scanner(System.in);
-                int testCases = sc.nextInt();
-            
-                for(int i=0; i<testCases; i++){
-                        System.out.println(solve(sc.nextInt()));
-                }
+        for(int i=0; i<testCases; i++){
+            System.out.println(sum(sc.nextInt()));
         }
+    }
+    
+    public static int sum(int n){
+        if(n<2){
+            return 1;
+        }
+        else{
+            return (n^2 - (n-1)^2) + sum(n-1);
+        }
+    }
 }
