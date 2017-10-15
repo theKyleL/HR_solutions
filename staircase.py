586a88d486daee245a3b00af3a76902ef1a5dbce
jag <kyle.latino@gmail.com>
2017-10-15 11:57:01 -0700

    Added environment Added encoding

--- staircase.py
+++ staircase.py
@@ -1,18 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 # HackerRank
 # Create a right-justified staircase with ‘spaces’ and ‘#’
 # Input is an integer for the number of lines to print.
 # Note: Last line is not preceeded with any spaces
 
-import sys
 
-n = int(input().strip())
-spaces = ""
-hashes = "#"
-
-for i in range(n):
+n = int(input())
+if type(n) is int:
 	spaces = ""
-	for i in range(n-1-i):
-		spaces += " "
+	hashes = "#"
+
+	for i in range(n):
+		spaces = ""
+		for i in range(n-1-i):
+			spaces += " "
 		
-	print(spaces + hashes)
-	hashes += "#"
+		print(spaces + hashes)
+		hashes += "#"
