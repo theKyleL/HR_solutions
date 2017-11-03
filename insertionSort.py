358a895821ee16ee55d93e0f0c86db389abfe7d7
Kyle Latino <kyle.latino@gmail.com>
2017-11-02 21:40:34 -0700

    edited insertionSort.py
    Added comments
    Modified to allow importing w/out running

--- insertionSort.py
+++ insertionSort.py
@@ -1,15 +1,29 @@
+"""
+Hackerrank problem "Insertion sort"
+Solution by Kyle Latino
+"""
+__author__ = 'Kyle Latino'
+
+
+# global variables
 arr = 0
 e = 0
 sorted = False
 
 
 def initialize():
+	"""
+	Set all variables from STDIN
+	"""
 	global arr, e, sorted
 	e = int(input()) - 1  # location of the inserted element
 	arr = list(map(int, input().strip().split()))
 
 
 def sort():
+	"""
+	Check order and handle sorting
+	"""
 	global arr, e, sorted
 	if arr[e] < arr[e-1]:
 		swapSpots()
@@ -18,7 +32,10 @@ def sort():
 
 
 def swapSpots():
-	global arr, e, sorted
+	"""
+	Perform variable swap
+	"""
+	global arr, e
 	temp = arr[e]
 	arr[e] = arr[e-1]
 	printList()
@@ -27,13 +44,17 @@ def swapSpots():
 
 
 def printList():
+	"""
+	Print the value of 'arr' to STDOUT.
+	"""
 	global arr
 	print(' '.join(str(val) for val in arr))
 
 
-initialize()
+if __name__ == '__main__':
+	initialize()
 
-while not sorted and e > 0:
-	sort()
+	while not sorted and e > 0:
+		sort()
 
-printList()
+	printList()
