482802a6d96a35ca4425f4817c3a727e19a40cec
Kyle Latino <kyle.latino@gmail.com>
2018-04-14 13:41:21 -0700

    added makingAnagrams.py
    Work in progress...

---
+++ makingAnagrams.py
@@ -0,0 +1,8 @@
+"""
+HackerRank Example problem Arrays: Strings: Making Anagrams
+Solution by: Kyle Latino
+
+Problem: 
+	Given two strings, a and B, that may or may not be of the same length, determine the minimum number of character deletions required to make a and b anagrams. Any characters can be deleted from either of the strings. 
+"""
+
